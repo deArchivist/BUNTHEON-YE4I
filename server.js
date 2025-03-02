325a9424f4f18d2ecf0782f944cbfc03cf98bac7
deArchivist <dearchivist18@gmail.com>
2025-03-02 16:48:59 +0700

    123

--- server.js
+++ server.js
@@ -2,24 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Basic security headers
-app.use((req, res, next) => {
-  res.set('X-Content-Type-Options', 'nosniff');
-  next();
-});
-
-// Serve static files
+// Serve static files from the React build
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Catch-all handler
+// Handle requests for the app
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Remove x-powered-by header
-app.disable('x-powered-by');
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
